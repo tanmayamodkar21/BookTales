cba6f8b85c9f552548eecf085b182542137d677e
TanmayAmodkar21 <tanuamodkar@gmail.com>
2024-04-08 10:04:09 +0530

    changes in services

--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -12,28 +12,28 @@ const OurServices = () => {
           {services_data.title}
         </h1>
       </section>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 px-10 pt-5">
-        <div className="flex flex-col items-center">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-28 px-10 pt-5">
+        <div className="flex flex-col items-center bg-white rounded-md">
           <img src={collect_data} alt="" className="services-cols-img" />
-          <p className="text-left text-lg font-normal px-52 pt-9 text-black tracking-wide">
+          <p className="text-left text-lg font-normal px-52 pt-9 text-black tracking-wide pb-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
             fugiat aperiam nobis eaque vero. Architecto fuga illo ipsum,
             consequatur quam quidem laudantium amet, aspernatur soluta pariatur
             esse ex odit consectetur?
           </p>
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center bg-white rounded-md">
           <img src={done_checking} alt="" className="services-cols-img" />
-          <p className="text-left text-lg font-normal px-52 pt-9 text-black tracking-wide">
+          <p className="text-left text-lg font-normal px-52 pt-9 text-black tracking-wide pb-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
             fugiat aperiam nobis eaque vero. Architecto fuga illo ipsum,
             consequatur quam quidem laudantium amet, aspernatur soluta pariatur
             esse ex odit consectetur?
           </p>
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center bg-white rounded-md">
           <img src={show_data} alt="" className="services-cols-img" />
-          <p className="text-left text-lg font-normal px-52 pt-9 text-black tracking-wide">
+          <p className="text-left text-lg font-normal px-52 pt-9 text-black tracking-wide pb-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
             fugiat aperiam nobis eaque vero. Architecto fuga illo ipsum,
             consequatur quam quidem laudantium amet, aspernatur soluta pariatur
